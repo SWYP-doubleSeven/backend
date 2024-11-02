084bda92c7a2a8caa6fbbc6c0d62023b29027d38
orchidHYE <trolly00@naver.com>
2024-11-02 15:07:37 +0900

    git init

---
+++ src/main/java/com/swyp/doubleSeven/DoubleSevenApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.doubleSeven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleSevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleSevenApplication.class, args);
+	}
+
+}
