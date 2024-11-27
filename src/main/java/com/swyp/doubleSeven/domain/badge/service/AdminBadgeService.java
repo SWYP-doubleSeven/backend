959c5710a2c7d8f17d61088e8de43edd90c2136e
leedawon <dawonLeeee@github.com>
2024-11-27 21:29:44 +0900

    관리자, 사용자 뱃지 기능 분리

--- src/main/java/com/swyp/doubleSeven/domain/badge/service/AdminBadgeService.java
+++ src/main/java/com/swyp/doubleSeven/domain/badge/service/AdminBadgeService.java
@@ -1,28 +1,27 @@
-package com.swyp.doubleSeven.domain.badge.dao;
+package com.swyp.doubleSeven.domain.badge.service;
 
 import com.swyp.doubleSeven.domain.badge.dto.request.BadgeRequest;
 import com.swyp.doubleSeven.domain.badge.dto.request.BadgeSearchCriteria;
 import com.swyp.doubleSeven.domain.badge.dto.response.BadgeResponse;
-import org.apache.ibatis.annotations.Mapper;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import java.util.Map;
 
-@Mapper
-public interface BadgeDAO {
+public interface AdminBadgeService {
 
     // 뱃지 등록
-    void insertBadge(BadgeRequest badgeRequest);
+    BadgeResponse insertBadge(BadgeRequest badgeRequest);
 
     // 뱃지 수정
-    int updateBadge(BadgeRequest badgeRequest);
+    BadgeResponse updateBadge(BadgeRequest badgeRequest);
 
     // 뱃지 삭제
     int deleteBadge(Integer badgeId);
 
-    // 뱃지 단건 조회
+    // 뱃지 단건조회
     BadgeResponse getBadge(Integer badgeId);
 
-    // 전체 뱃지 목록 조회
+    // 뱃지 목록 조회
     List<BadgeResponse> getBadgeList(BadgeSearchCriteria criteria);
+
 }
---
+++ src/main/java/com/swyp/doubleSeven/domain/badge/service/UserBadgeServiceImpl.java
@@ -0,0 +1,31 @@
+package com.swyp.doubleSeven.domain.badge.service;
+
+import com.swyp.doubleSeven.domain.badge.dao.UserBadgeDAO;
+import com.swyp.doubleSeven.domain.badge.dto.request.BadgeRequest;
+import com.swyp.doubleSeven.domain.badge.dto.response.BadgeResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserBadgeServiceImpl implements UserBadgeService {
+
+    private final UserBadgeDAO userBadgeDAO;
+
+    @Override
+    public BadgeResponse getBadge(BadgeRequest request) {
+        return userBadgeDAO.getBadge(request);
+    }
+
+    // 뱃지 목록 조회
+    @Override
+    public List<BadgeResponse> getBadgeList(Integer memberId) {
+        return userBadgeDAO.getBadgeList(memberId);
+    }
+
+
+}
