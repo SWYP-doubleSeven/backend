f330b73ccf02c67e219d996d2a32e83cc3f9772c
InimitablePioneer <seungwonpioneer@gmail.com>
2024-11-29 14:00:35 +0900

    NOV29 at work via WIN

---
+++ src/main/java/com/swyp/doubleSeven/domain/member/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.swyp.doubleSeven.domain.member.controller;
+
+import com.swyp.doubleSeven.domain.member.login.LoginForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class LoginController {
+
+//    @GetMapping("api/login")
+//    public String loginForm(@ModelAttribute LoginForm loginForm) {
+//        return null; // 추후 프론트단 화면에 연결할 예정
+//    }
+
+
+    @GetMapping()
+    public
+}
--- src/main/java/com/swyp/doubleSeven/domain/member/login/LoginServiceImpl.java
+++ src/main/java/com/swyp/doubleSeven/domain/member/login/LoginServiceImpl.java
@@ -1,6 +1,15 @@
 package com.swyp.doubleSeven.domain.member.login;
 
-public class LoginServiceImpl { //필드에 MemberService 받아서 Mybatis xml 접근
+import com.swyp.doubleSeven.domain.member.Member;
+import com.swyp.doubleSeven.domain.member.service.MemberService;
+
+public class LoginServiceImpl implements LoginService{ //필드에 MemberService 받아서 Mybatis xml 접근
+
+    private MemberService memberService;
 
 
+    @Override
+    public Member login(Member member) {
+        return null;
+    }
 }
