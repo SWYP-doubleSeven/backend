1b98a1bb40f8747c83a8240f9a75d4769c1d844f
leedawon <dawonLeeee@github.com>
2024-12-02 15:41:21 +0900

    로그인기능 설정 temp3

--- src/main/java/com/swyp/doubleSeven/domain/member/service/KakaoApiClient.java
+++ src/main/java/com/swyp/doubleSeven/domain/member/service/KakaoApiClient.java
@@ -3,6 +3,7 @@ package com.swyp.doubleSeven.domain.member.service;
 import com.swyp.doubleSeven.domain.member.dto.response.KakaoMemberResponse;
 import com.swyp.doubleSeven.domain.member.dto.response.KakaoTokenResponse;
 import com.swyp.doubleSeven.domain.member.dto.response.KakaoUserDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
@@ -10,6 +11,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@Slf4j
 public class KakaoApiClient {
     private final RestTemplate restTemplate = new RestTemplate();
 
@@ -22,15 +24,20 @@ public class KakaoApiClient {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", "489a2f33bf9d90c59950291ca077adc9");
-//        params.add("redirect_uri", "http://localhost:8090/api/auth/kakao-login");
-        params.add("redirect_uri", "http://3.39.123.15:8090/api/auth/kakao-login");
+        params.add("redirect_uri", "http://localhost:8090/api/auth/kakao-login");
+//        params.add("redirect_uri", "http://3.39.123.15:8090/api/auth/kakao-login");
         params.add("code", authorizationCode);
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
 
-        ResponseEntity<KakaoTokenResponse> response = restTemplate.postForEntity(url, request, KakaoTokenResponse.class);
+        try {
+            ResponseEntity<KakaoTokenResponse> response = restTemplate.postForEntity(url, request, KakaoTokenResponse.class);
+            return response.getBody().getAccessToken();
+        } catch(Exception e) {
+            log.error("카카오 api호출 실패 : ", e);
+            throw new RuntimeException("카카오api호출 실패", e);
+        }
 
-        return response.getBody().getAccessToken();
     }
 
     public KakaoUserDTO getUserInfo(String accessToken) {
