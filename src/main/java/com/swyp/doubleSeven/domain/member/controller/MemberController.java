eaca8c1dc8ab4309c3cb90b9da42fad040010e73
leedawon <dawonLeeee@github.com>
2024-12-02 14:52:54 +0900

    local일때 memberId 강제설정

--- src/main/java/com/swyp/doubleSeven/domain/member/controller/MemberController.java
+++ src/main/java/com/swyp/doubleSeven/domain/member/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.swyp.doubleSeven.domain.member.controller;
 
+import com.swyp.doubleSeven.common.util.CommonUtil;
 import com.swyp.doubleSeven.domain.member.dto.response.MemberResponse;
 import com.swyp.doubleSeven.domain.member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +16,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/kakao-login")
-    public ResponseEntity<String> kakaoLogin(@RequestParam("code") String code, HttpSession session) {
+    public ResponseEntity<MemberResponse> kakaoLogin(@RequestParam("code") String code, HttpSession session) {
         /* step 1: 프론트에서 다음 url을 통해 호출
         * https://kauth.kakao.com/oauth/authorize?client_id=489a2f33bf9d90c59950291ca077adc9&redirect_uri=http://localhost:8090/api/auth/kakao-login&response_type=code
         * */
@@ -25,7 +27,6 @@ public class MemberController {
         session.setAttribute("memberNickname", memberResponse.getMemberNickname());
         session.setAttribute("loginType", memberResponse.getLoginType());
         session.setAttribute("role", memberResponse.getRole());
-        // todo : response같이 전달(-)
-        return ResponseEntity.ok("Kakao login successful");
+        return ResponseEntity.ok(memberResponse);
     }
 }
