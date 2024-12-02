ed6b01a62f2b76f709483054035725523b9895d4
leedawon <dawonLeeee@github.com>
2024-12-02 15:18:32 +0900

    로그인기능 설정 temp

--- src/main/java/com/swyp/doubleSeven/domain/member/controller/MemberController.java
+++ src/main/java/com/swyp/doubleSeven/domain/member/controller/MemberController.java
@@ -6,12 +6,16 @@ import com.swyp.doubleSeven.domain.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
     private final MemberService memberService;
 
@@ -21,12 +25,14 @@ public class MemberController {
         * https://kauth.kakao.com/oauth/authorize?client_id=489a2f33bf9d90c59950291ca077adc9&redirect_uri=http://localhost:8090/api/auth/kakao-login&response_type=code
         * */
         String accessToken = memberService.getKakaoAccessToken(code);
+        log.debug("1번 실행");
         MemberResponse memberResponse = memberService.processKakaoUser(accessToken);
 
         session.setAttribute("memberId", memberResponse.getMemberId());
         session.setAttribute("memberNickname", memberResponse.getMemberNickname());
         session.setAttribute("loginType", memberResponse.getLoginType());
         session.setAttribute("role", memberResponse.getRole());
+        log.debug("7번 실행");
         return ResponseEntity.ok(memberResponse);
     }
 }
