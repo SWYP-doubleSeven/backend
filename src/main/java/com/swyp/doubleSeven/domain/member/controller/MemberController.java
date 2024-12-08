e0e80daf093d729a4562ceecdc876d7704ae5092
leedawon <dawonLeeee@github.com>
2024-12-08 16:35:00 +0900

    카카오로그인 테스트, application.yml정보 수정

--- src/main/java/com/swyp/doubleSeven/domain/member/controller/MemberController.java
+++ src/main/java/com/swyp/doubleSeven/domain/member/controller/MemberController.java
@@ -1,38 +1,45 @@
 package com.swyp.doubleSeven.domain.member.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.swyp.doubleSeven.common.aspect.anotation.AuthCheck;
 import com.swyp.doubleSeven.common.util.CommonAspect;
+import com.swyp.doubleSeven.common.util.CommonImageUploader;
+import com.swyp.doubleSeven.domain.badge.dto.request.BadgeRequest;
 import com.swyp.doubleSeven.domain.badge.dto.response.BadgeResponse;
+import com.swyp.doubleSeven.domain.common.enums.Role;
+import com.swyp.doubleSeven.domain.member.dto.request.MemberRequest;
 import com.swyp.doubleSeven.domain.member.dto.response.MemberResponse;
 import com.swyp.doubleSeven.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
     private final MemberService memberService;
     private final CommonAspect commonAspect;
+    private final CommonImageUploader commonImageUploader;
 
-    @GetMapping("/kakao-login")
-    public ResponseEntity<MemberResponse> kakaoLogin(@RequestParam("code") String code, HttpSession session, HttpServletRequest httpServletRequest) {
-        /* step 1: 프론트에서 다음 url을 통해 호출
-        * https://kauth.kakao.com/oauth/authorize?client_id=489a2f33bf9d90c59950291ca077adc9&redirect_uri=http://localhost:8090/api/auth/kakao-login&response_type=code
-        * */
-        // 로그 추가
-        log.info("Received Authorization Code: {}", code);
-        log.info("Redirect URI Expected: http://3.39.123.15:8090/api/auth/kakao-login");
+    @GetMapping("/auth/kakao-login")
+    public ResponseEntity<MemberResponse> kakaoLogin(@RequestParam("code") String code, HttpSession session) {
 
-
-        String accessToken = memberService.getKakaoAccessToken(code, httpServletRequest);
-        MemberResponse memberResponse = memberService.processKakaoUser(accessToken);
+//      String accessToken = memberService.getKakaoAccessToken(code, httpServletRequest);
+        MemberResponse memberResponse = memberService.processKakaoUser(code);
 
         session.setAttribute("memberId", memberResponse.getMemberId());
         session.setAttribute("memberNickname", memberResponse.getMemberNickname());
@@ -44,4 +51,36 @@ public class MemberController {
 
         return ResponseEntity.ok(memberResponse);
     }
+
+//    @PostMapping("/users")
+//    @Operation
+//    @AuthCheck(allowedRoles = Role.MEMBER)
+//    public ResponseEntity<MemberResponse> updateMemberInfo(
+//            @RequestPart(value = "memberRequest") String memberRequestJson,
+//            @RequestPart(value = "profileImage", required = false) MultipartFile multipartFile
+//    ) {
+//        // JSON 파싱
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        MemberRequest memberRequest;
+//        try {
+//            memberRequest = objectMapper.readValue(memberRequestJson, MemberRequest.class);
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//        }
+//
+//        String fileName = "";
+//        if(multipartFile != null && !multipartFile.isEmpty()) {
+//            try {
+//                fileName = commonImageUploader.upload(multipartFile, "profile_image");
+//                memberRequest.set
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                        .body(null); // 업로드 실패 처리
+//            }
+//        }
+//
+//        MemberResponse memberResponse = memberService.updateMemberInfo(memberRequest);
+//        return ResponseEntity.ok().body(memberResponse);
+//    }
 }
