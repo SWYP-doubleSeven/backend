d75ca02dea3b5a2b96e390062805f2777c7137b9
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2024-12-10 22:01:18 +0900

    회원탈퇴, 장기미사용 회원 삭제 메서드 추가

--- src/main/java/com/swyp/doubleSeven/common/util/CommonSchedular.java
+++ src/main/java/com/swyp/doubleSeven/common/util/CommonSchedular.java
@@ -1,9 +1,23 @@
 package com.swyp.doubleSeven.common.util;
 
+import com.swyp.doubleSeven.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class CommonSchedular {
 
-    //
+    private final MemberService memberService;
+
+    // 매일 자정에 비활성 회원 삭제
+    @Scheduled(cron = "0 0 0 * * *") // 매일 자정 실행
+    public void deleteInactiveMembersTask() {
+        memberService.deleteOldMember();
+        log.info("Inactive members deleted successfully at: " + java.time.LocalDateTime.now());
+    }
+
 }
